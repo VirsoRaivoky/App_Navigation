e95dab8068c18f8eb3cc143a761918b1e78491ea
Wilson Xavier <87943439+VirsoRaivoky@users.noreply.github.com>
2023-05-25 23:06:13 -0300

    Delete Task.kt

--- app/src/main/java/com/example/navigation_aula/models/Task.kt
+++
@@ -1,8 +0,0 @@
-package com.example.navigation_aula.models
-
-data class Task(
-    val name: String = "",
-    val review: String = "",
-    val isFavorite: Boolean = false,
-    var isFiltered: Boolean = false
-)
